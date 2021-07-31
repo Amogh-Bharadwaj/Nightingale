0aef8abfad9806c0d771e765fd1eaf34f279c4ce
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-07-31 21:47:49 +0530

    Postponing protected routing

--- src/pages/maindesk/desk.jsx
+++ src/pages/maindesk/desk.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { useState } from "react";
 import { useEffect } from "react";
 import { withRouter } from "react-router";
+import { useHistory } from "react-router";
+import { Redirect } from "react-router";
 
 const Desk=()=>{
-
-    return(
+   const history=useHistory();
+   return(
         <div><p>HELLOOOOOOO</p></div>
     )
 }
